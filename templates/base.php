71f2dc4ee72053f84f87a6365fed530e889c966b
Pedro Rey Anca <personal@peprolinbot.com>
2022-01-05 00:59:32 +0100

    Change project info

--- templates/base.php
+++ templates/base.php
@@ -20,8 +20,8 @@
     <div class="section">
         <div class="status-footer">
             <p>
-                <a href="https://github.com/styxit/MinecraftStatusPage"><i class="fa fa-github"></i> View on GitHub</a>
-                <a href="https://styxit.com"><i class="fa fa-user-circle"></i> Created by Styxit</a>
+                <a href="https://github.com/peprolinbot/mc-status"><i class="fa fa-github"></i> View on GitHub</a>
+                <a href="https://peprolinbot.com"><i class="fa fa-user-circle"></i> Created by peprolinbot</a>
             </p>
         </div>
     </div>
